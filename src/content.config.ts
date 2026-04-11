e3ccc1c240a15e7c75193b2cfbc35e58327858f0
Erdhy Ernando <ernandodavis1002@gmail.com>
2026-04-11 22:01:28 +0200

    config: setup content collection with zod

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    date: z.coerce.date(),
+    thumbnail: z.string(),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collection = { blog };
